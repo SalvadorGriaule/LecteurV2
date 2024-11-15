1a746f910315ca9e92729bfc67454f5e7cded2db
SalvadorGriaule <morales.quentin@gmx.fr>
2024-11-15 16:53:56 +0100

    change 15/11

--- asset/js/modules/initMusicBrainz.js
+++ asset/js/modules/initMusicBrainz.js
@@ -1,4 +1,14 @@
-export const initInfo = async () => {
+const loadSVG = async (filename) => {
+    let result;
+    const content = document.createElement("div")
+    content.classList.add("svgContent")
+    result = await fetch("./asset/img/svg/"+filename).then((val) => {return val.text()}).then((val) => {
+        content.innerHTML = val.substring(val.indexOf("<svg"));
+    })
+    return content
+}
+
+const initInfo = async () => {
     let artist = tracklist[currentImg].auteur;
     let id = await fetch("https://musicbrainz.org/ws/2/artist/?query=" + artist + "&fmt=json").then((val) => { return val.json() }).then((val) => { return val.artists[0].id })
     let album = await fetch("https://musicbrainz.org/ws/2/release-group?artist=" + id + "&fmt=json&type=album").then((val) => { return val.json() }).then((val) => { return val["release-groups"] });
@@ -12,6 +22,23 @@ export const initInfo = async () => {
         pInsert.textContent = album[i].title;
         div.append(pInsert)
     }
-    document.getElementById("side").append(div)
+    
+    if(!document.getElementById("side")){
+        const side = document.createElement("section");
+        side.id = "side";
+        document.getElementById("playlist").after(side)   
+    }
+    const sideBut = document.createElement("div")
+    const svg = await loadSVG("onglet.svg")
+    side.append(sideBut)
+    sideBut.id = "sideButton"
+    sideBut.append(svg);
+    svg.querySelector("path").style.fill = "#d63501"
+    console.log();
+    
+    side.append(div)
+
 }
+
+export {loadSVG,initInfo}
 
